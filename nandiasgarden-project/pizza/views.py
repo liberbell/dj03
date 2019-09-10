32ae41a20e2a09f2d5d74db7232d7aae5a6ec4a0
Liberbell <liberbell@gmail.com>
2019-09-11 08:41:15 +0900

    views1

--- nandiasgarden-project/pizza/views.py
+++ nandiasgarden-project/pizza/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'pizza/home.html')
+    return render(request, 'home.html')
 
 def order(request):
     return render(request, 'pizza/order.html')
