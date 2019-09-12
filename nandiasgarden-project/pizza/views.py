dd823956092c089af0d3b7331df5e6a2cb19f57e
Liberbell <liberbell@gmail.com>
2019-09-13 07:35:34 +0900

    views1

--- nandiasgarden-project/pizza/views.py
+++ nandiasgarden-project/pizza/views.py
@@ -8,6 +8,10 @@ def home(request):
 def order(request):
     if request.method == 'POST':
         filled_form = PizzaForm(request.POST)
+        if filled_form.is_vallid():
+            note = 'Thanks for ordering! Your %s %s and %s pizza is on its way.' %(filled_form.cleaned_data['size'],
+            %(filled_form.cleaned_data['topping1'],
+            %(filled_form.cleaned_data['topping2'])
     else:
         form = PizzaForm()
         return render(request, 'pizza/order.html', {'pizzaform': form})
