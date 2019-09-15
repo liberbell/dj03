7483b444b50bff49cb90b01479e6538978a1c227
Liberbell <liberbell@gmail.com>
2019-09-15 11:23:26 +0900

    forms1

--- nandiasgarden-project/pizza/forms.py
+++ nandiasgarden-project/pizza/forms.py
@@ -10,7 +10,7 @@ from .models import Pizza, Size
 class PizzaForm(forms.ModelForm):
 
     # size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
-    images = forms.ImageField()
+    # images = forms.ImageField()
     class Meta:
         model = Pizza
         fields = ['topping1', 'topping2', 'size']
