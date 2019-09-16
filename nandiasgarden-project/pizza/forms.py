aedb5444aeaf60201fab6ab84ce6713f73c79af2
Liberbell <liberbell@gmail.com>
2019-09-17 07:35:05 +0900

    forms1

--- nandiasgarden-project/pizza/forms.py
+++ nandiasgarden-project/pizza/forms.py
@@ -11,6 +11,7 @@ class PizzaForm(forms.ModelForm):
 
     # size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
     # images = forms.ImageField()
+    email = forms.EmailField()
     class Meta:
         model = Pizza
         fields = ['topping1', 'topping2', 'size']
